0d42a253cb91b74a71ea29f6fe95365d7e572eef
hectick <chyeon1223@cau.ac.kr>
2022-09-30 19:28:57 +0900

    FEAT : Add MailAccountResponse class
    - 각 메일 계정의 정보를 예민한 정보를 제외하고 다뤄야 할 때 사용하기 위해 만든 클래스임

---
+++ everydayMail/src/main/java/com/mkcy/everydaymail/account/MailAccountResponse.java
@@ -0,0 +1,45 @@
+package com.mkcy.everydaymail.account;
+/*
+public class MailAccountResponse {
+	//private String id;
+	private int registerId;
+	//private String mailHost;
+	private String mailAddr;
+	private String registerDate;
+	private int deletedMails;
+	
+	public int getRegisterId() {
+		return registerId;
+	}
+	
+	public void setRegisterId(int registerId) {
+		this.registerId = registerId;
+	}
+	
+	public String getMailAddr() {
+		return mailAddr;
+	}
+	
+	public void setMailAddr(String mailAddr) {
+		this.mailAddr = mailAddr;
+	}
+	
+	public String getRegisterDate() {
+		return registerDate;
+	}
+	
+	public void setRegisterDate(String registerDate) {
+		this.registerDate = registerDate;
+	}
+	
+	public int getDeletedMails() {
+		return deletedMails;
+	}
+	
+	public void setDeletedMails(int deletedMails) {
+		this.deletedMails = deletedMails;
+	}
+	
+
+}
+*/
